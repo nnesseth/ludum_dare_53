588674470e21c6afbe98d024e836b006f33cf92c
c-usher <usher.cody@outlook.com>
2023-03-11 19:08:51 -0700

    Hero Jumps
    Hero can Jump

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour
 {
-    public float velocity = 5f;
+    public float velocity = 5f; // movement speed
+    public float jumpStrength = 5f; // how high
+    private bool onFloor = true;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody heroRb;
 
     void Start()
     {
-        
+        heroRb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -21,5 +24,20 @@ public class HeroMovement : MonoBehaviour
         //Time.deltaTime updates over time to compensate for different machine speeds
         transform.Translate(Vector3.forward * Time.deltaTime * velocity * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * velocity * horizontalInput);
+
+        // Jump logic
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+            heroRb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            onFloor = false; // Impulse adds instant force.
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            onFloor = true;
+        }
+    }    
 }
