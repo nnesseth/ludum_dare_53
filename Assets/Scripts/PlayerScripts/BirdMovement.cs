c0a0e2e9084bb15a7b729c0dc293a6299514ed78
c-usher <usher.cody@outlook.com>
2023-04-30 20:12:15 -0600

    more flying esque

--- Assets/Scripts/PlayerScripts/BirdMovement.cs
+++ Assets/Scripts/PlayerScripts/BirdMovement.cs
@@ -15,36 +15,37 @@ public class BirdMovement : MonoBehaviour
     
     void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * forwardMovement, Space.World);
-        if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow)))
-        {
-            if (this.gameObject.transform.position.x > Boundaires.leftSide) 
-            {
+        transform.position += transform.forward * Time.deltaTime * 5f;
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+        // if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow)))
+        // {
+        //     if (this.gameObject.transform.position.x > Boundaires.leftSide) 
+        //     {
 
-                transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed); 
-            }         
-        }
-        if (Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow)))
-        {
-            if (this.gameObject.transform.position.x < Boundaires.rightSide) 
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed * -1);
-            }
-        }
-        if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow)))
-        {
-            if (this.gameObject.transform.position.y < Boundaires.topSide)
-            {
-                transform.position += Vector3.up *  Time.deltaTime * strafeSpeed;
-            }
-        }
-        if (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow)))
-        {
-            if (this.gameObject.transform.position.y > Boundaires.bottomSide)
-            {
-                transform.position += Vector3.down * Time.deltaTime * strafeSpeed;
-            }
+        //         transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed); 
+        //     }         
+        // }
+        // if (Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow)))
+        // {
+        //     if (this.gameObject.transform.position.x < Boundaires.rightSide) 
+        //     {
+        //         transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed * -1);
+        //     }
+        // }
+        // if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow)))
+        // {
+        //     if (this.gameObject.transform.position.y < Boundaires.topSide)
+        //     {
+        //         transform.position += Vector3.up *  Time.deltaTime * strafeSpeed;
+        //     }
+        // }
+        // if (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow)))
+        // {
+        //     if (this.gameObject.transform.position.y > Boundaires.bottomSide)
+        //     {
+        //         transform.position += Vector3.down * Time.deltaTime * strafeSpeed;
+        //     }
             
-        }
+        // }
     }
 }
