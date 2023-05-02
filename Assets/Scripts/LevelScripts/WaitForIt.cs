962fc213ef8ded9694d774b31b7ec5c9fca8127d
nnesseth <68468669+nnesseth@users.noreply.github.com>
2023-05-02 01:14:06 -0400

    ComMMit

--- Assets/Scripts/LevelScripts/WaitForIt.cs
+++ Assets/Scripts/LevelScripts/WaitForIt.cs
@@ -8,25 +8,18 @@ public class WaitForIt : MonoBehaviour
 {
     public float start;
     public float timeToWait = 32.0F;
-    public bool enter = false;
-    private VideoPlayer player;
 
     // Start is called before the first frame update
     void Start()
     {
         start = Time.time;
-        player = GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)){
-            enter = true;
-            player.Play();
-        }
 
-        if (enter && timeToWait < (Time.time - start))
+        if (timeToWait < (Time.time - start))
         {
             SceneManager.LoadScene("One");
         }
--- Assets/Scripts/LevelScripts/WaitForIt2.cs
+++ Assets/Scripts/LevelScripts/WaitForIt2.cs
@@ -8,26 +8,17 @@ public class WaitForIt2 : MonoBehaviour
 {
     public float start;
     public float timeToWait = 32.0F;
-    public bool enter = false;
-    private VideoPlayer player;
 
     // Start is called before the first frame update
     void Start()
     {
         start = Time.time;
-        player = GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return)) 
-        {
-            enter = true;
-            player.Play();
-        }
-
-        if (enter && timeToWait < (Time.time - start))
+        if (timeToWait < (Time.time - start))
         {
             SceneManager.LoadScene("Credits");
         }
