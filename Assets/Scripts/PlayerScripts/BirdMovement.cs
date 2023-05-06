782d8c39fbd1d7e475976541496c3d3e5f5e05de
c-usher <usher.cody@outlook.com>
2023-05-06 14:47:06 -0600

    movement working speed needs tweaked

--- Assets/Scripts/PlayerScripts/BirdMovement.cs
+++ Assets/Scripts/PlayerScripts/BirdMovement.cs
@@ -9,24 +9,20 @@ public class BirdMovement : MonoBehaviour
     private float strafeSpeed = 20000;
 
     [SerializeField]
-    private float speed = 10;
+    private float speed = 200000;
 
     [SerializeField]
     ForceMode mode;
 
-    // private Vector3 dirInput;
-    private Rigidbody birdBody;
     Rigidbody birdRb;
 
      private void Awake()
      {
-         TryGetComponent(out birdBody);
          birdRb = GetComponent<Rigidbody>();
      }
 
     void FixedUpdate()
     {
-          // birdBody.velocity = new Vector3(0, 0, speed * Time.deltaTime); 
           birdRb.AddForce(transform.forward * speed * Time.deltaTime);
           transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
           if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow)))
@@ -36,30 +32,6 @@ public class BirdMovement : MonoBehaviour
           if(Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow)))
           {
             birdRb.AddForce(transform.right * strafeSpeed * Time.deltaTime);
-          }
-        //   if(Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow)))
-        //   {
-        //     birdRb.AddForce(transform.up * -strafeSpeed );
-        //   }
-        //   if(Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow)))
-        //   {
-        //     birdBody.velocity = new Vector3(0, speed * Time.deltaTime,  speed * Time.deltaTime);
-        //   }
-        //   if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)))
-        //   {
-        //     birdBody.velocity = new Vector3(-speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime); 
-        //   }
-        //   if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)))
-        //   {
-        //     birdBody.velocity = new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime); 
-        //   }
-        //   if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)))
-        //   {
-        //     birdBody.velocity = new Vector3(speed * Time.deltaTime, -speed * Time.deltaTime, speed * Time.deltaTime); 
-        //   }
-        //   if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)))
-        //   {
-        //     birdBody.velocity = new Vector3(-speed * Time.deltaTime, -speed * Time.deltaTime, speed * Time.deltaTime); 
-        //   }         
+          }       
     }
 }
